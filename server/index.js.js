65d6cdece8cdc722a7dc535e2e880e1c27411c13
pavankalyan <114129248+pavanhoney630@users.noreply.github.com>
2025-01-23 08:39:30 +0530

    commit

--- server/index.js.js
+++ server/index.js.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
 const cors = require('cors')
 const authRoutes = require('./routes/AuthRoutes');
 const app = express();
@@ -11,13 +10,14 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 const corsOptions = {
   origin: "https://url-shortend-client.vercel.app", // URL of your React frontend
-  methods: ["GET", "POST"], // HTTP methods allowed
+  methods: ["GET", "POST", "OPTIONS"], // HTTP methods allowed
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allow cookies to be sent
 };
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
+
 
 app.get('/', (req,res)=>{
   res.send("api is working fine");
@@ -30,4 +30,4 @@ mongoose.connect(process.env.MONGO_URI)
 // Routes
 app.use('/api', authRoutes)
   // Start server
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT , () => console.log(`Server running on port ${PORT}`));
