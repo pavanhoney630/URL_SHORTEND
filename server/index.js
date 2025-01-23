7db7e9af1f0c63a5b7d1eb964eb8336babdd76fe
pavankalyan <114129248+pavanhoney630@users.noreply.github.com>
2025-01-23 10:28:48 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
-require('dotenv').config();
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors')
 const authRoutes = require('./routes/AuthRoutes');
-const app = express();
 
 
-// Middleware
+require('dotenv').config();
+
+app.get('/ping', (req, res) => {
+  res.send('PONG');
+});
+
 app.use(express.json());
 const corsOptions = {
   origin: ["https://url-shortend-client.vercel.app","http://localhost:3000"], // URL of your React frontend
@@ -14,20 +18,18 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
+app.use(bodyParser.json());
+app.use('/api', authRoutes)
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
-
-app.get('/ping', (req,res)=>{
-  res.send("api is working fine");
-})
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
 // Routes
-app.use('/api', authRoutes)
+
   // Start server
   const PORT = process.env.PORT || 5000;
   app.listen(PORT , () => console.log(`Server running on port ${PORT}`));
