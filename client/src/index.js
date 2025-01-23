42606bcd7893950ba82fc6dfd65d24f62bb8922f
pavankalyan <114129248+pavanhoney630@users.noreply.github.com>
2025-01-23 08:09:19 +0530

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './components/Signup'; 
 import Login from './components/Login';
 
@@ -9,10 +9,10 @@ import Login from './components/Login';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
+  <Router>
     <Routes>
       <Route path="/signup" element={<Signup />} /> 
       <Route path="/login" element={<Login />} />
     </Routes>
-  </BrowserRouter>
+  </Router>
 );
--- server/index.js.js
+++ server/index.js.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req,res)=>{
-  res.json("api is working fine");
+  res.send("api is working fine");
 })
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
